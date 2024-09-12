057fc66292175431b8a0e68989576a1f0288f621
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-09-12 20:28:36 +0500

    Update keyStagesText.jsx

--- src/components/mainPage/keyStages/keyStagesText.jsx
+++ src/components/mainPage/keyStages/keyStagesText.jsx
@@ -16,7 +16,7 @@ export const KeyStagesText = () => {
       </div>
 
       <div className="text-center gap-8 pt-3 mt-5 rounded-lg">
-        <p className="mt-7 text-md lg:text-lg  leading-8 text-gray-700">
+        <p className="mt-7 text-md lg:text-lg  font-bold leading-8 text-gray-700">
           Welcome To Al-Madinah Online Secondary School, An Islamic institution
           where academic excellence nurtures <br /> the growth of confident and
           compassionate individuals, Guided by the principles of Islam.
