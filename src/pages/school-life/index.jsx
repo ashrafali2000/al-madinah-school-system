26c217099870d0c2c817228d7e2f2813a88f2c7e
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-09-13 22:29:13 +0500

    Update index.jsx

--- src/pages/school-life/index.jsx
+++ src/pages/school-life/index.jsx
@@ -257,7 +257,7 @@ export default function indexSchoolLife() {
             <div>
               <img
                 className="-rotate-3 rounded-xl"
-                src="/school.jpg"
+                src="/School.jpg"
                 alt="image"
               />
             </div>
